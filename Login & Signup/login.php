0977a516a57d5c3092ea892bdeb87a14680430f6
Ahmed Abdel Gwwad <mnoomidoostar7@gmail.com>
2023-01-09 20:32:39 +0200

    first commit

---
+++ Login & Signup/login.php
@@ -0,0 +1,8 @@
+<?php
+$email = $_POST['email'];
+here $email is a variable we are getting email using $_POST global variable $_POST['email']; here email is the name of input field
+
+$con = new mysqli("localhost","root","","test");
+
+$con is a variable which hold the $con object
+?>
